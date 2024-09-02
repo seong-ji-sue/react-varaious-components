b0fcc9ce1dd602b054a2c3ce2d9ee6df60727640
jisue <tjdwltn121@gmail.com>
2024-09-03 00:40:43 +0900

    chore: page 제거 초기상태

--- packages/front/src/index.js
+++ packages/front/src/index.js
@@ -2,20 +2,18 @@ import React from 'react';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import {createRoot} from 'react-dom/client';
 import {GlobalStyle} from '@jsproject/common';
-import InputSelectField from './components/Contents/InputSelectField';
-import FieldTest from './pages/FieldTest';
 import Layout from './components/Layout/Layout';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <div>메인페이지</div>,
+		element: <Layout>메인페이지</Layout>,
 	},
 	{
-		path: '/input-select',
+		path: '/test',
 		element: (
 			<Layout>
-				<FieldTest />
+				<div>test</div>
 			</Layout>
 		),
 	},
