a937f7cfa47101ecb11fa5096cd946f3a5963521
jisue <tjdwltn121@gmail.com>
2025-08-20 18:27:57 +0900

    chore: 테이블 칼럼 sticky 적용중

--- packages/test-file-upload/src/component/Page/TableTest.js
+++ packages/test-file-upload/src/component/Page/TableTest.js
@@ -30,6 +30,90 @@ const data = [
 		createdTime: '2025-08-08T05:47:57.511Z',
 		creator: 'js',
 	},
+	{
+		id: 'test1',
+		name: 'gg',
+		description: 'rewrwr',
+		createdTime: '2025-08-08T05:47:57.511Z',
+		creator: 'js',
+	},
+	{
+		id: 'test2',
+		name: '1gg',
+		description: 'rewrwr',
+		createdTime: '2025-08-08T05:47:57.511Z',
+		creator: 'js',
+	},
+	{
+		id: 'test3',
+		name: 'g22g',
+		description: 'rewrwr',
+		createdTime: '2025-08-08T05:47:57.511Z',
+		creator: 'js',
+	},
+	{
+		id: 'test4',
+		name: 'gg333',
+		description: 'rewrwr',
+		createdTime: '2025-08-08T05:47:57.511Z',
+		creator: 'js',
+	},
+	{
+		id: 'test1',
+		name: 'gg',
+		description: 'rewrwr',
+		createdTime: '2025-08-08T05:47:57.511Z',
+		creator: 'js',
+	},
+	{
+		id: 'test2',
+		name: '1gg',
+		description: 'rewrwr',
+		createdTime: '2025-08-08T05:47:57.511Z',
+		creator: 'js',
+	},
+	{
+		id: 'test3',
+		name: 'g22g',
+		description: 'rewrwr',
+		createdTime: '2025-08-08T05:47:57.511Z',
+		creator: 'js',
+	},
+	{
+		id: 'test4',
+		name: 'gg333',
+		description: 'rewrwr',
+		createdTime: '2025-08-08T05:47:57.511Z',
+		creator: 'js',
+	},
+	{
+		id: 'test1',
+		name: 'gg',
+		description: 'rewrwr',
+		createdTime: '2025-08-08T05:47:57.511Z',
+		creator: 'js',
+	},
+	{
+		id: 'test2',
+		name: '1gg',
+		description: 'rewrwr',
+		createdTime: '2025-08-08T05:47:57.511Z',
+		creator: 'js',
+	},
+	{
+		id: 'test3',
+		name: 'g22g',
+		description: 'rewrwr',
+		createdTime: '2025-08-08T05:47:57.511Z',
+		creator: 'js',
+	},
+	{
+		id: 'test4',
+		name: 'gg333',
+		description: 'rewrwr',
+		createdTime: '2025-08-08T05:47:57.511Z',
+		creator: 'js',
+	},
 ];
 
 const lName = {value: 'name', label: '이름'};
@@ -45,7 +129,32 @@ const cDescription = {
 const cCreateTime = {accessorKey: lCreateTime.value, header: lCreateTime.label};
 const cCreator = {accessorKey: lCreator.value, header: lCreator.label};
 
-const columns = [cName, cDescription, cCreateTime, cCreator];
+const columns = [
+	cDescription,
+	cName,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cDescription,
+	cCreateTime,
+	cCreator,
+];
 
 const TableTest = () => {
 	const props = useMemo(
--- packages/test-file-upload/src/component/Table/index.js
+++ packages/test-file-upload/src/component/Table/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+// Table.jsx
+
+import React, {useState} from 'react';
 import {
 	flexRender,
 	getCoreRowModel,
@@ -7,41 +9,50 @@ import {
 import './Table.scss';
 
 const Table = ({data, columns}) => {
+	const [columnPinning, setColumnPinning] = useState({
+		left: ['name'],
+	});
 	const table = useReactTable({
 		data,
 		columns,
 		getRowId: (v) => v.id,
 		getCoreRowModel: getCoreRowModel(),
+		state: {
+			columnPinning,
+		},
+		onColumnPinningChange: setColumnPinning,
 	});
 
 	return (
-		<table>
-			<thead>
-				{table.getHeaderGroups().map((headerGroup) => (
-					<tr key={headerGroup.id}>
-						{headerGroup.headers.map((header) => (
-							<th key={header.id}>
-								{flexRender(
-									header.column.columnDef.header,
-									header.getContext(),
-								)}
-							</th>
-						))}
-					</tr>
-				))}
-			</thead>
-			<tbody>
-				{table.getRowModel().rows.map((row) => (
-					<tr key={row.id}>
-						{row.getVisibleCells().map((cell) => (
-							<td key={cell.id}>
-								{flexRender(cell.column.columnDef.cell, cell.getContext())}
-							</td>
-						))}
-					</tr>
-				))}
-			</tbody>
-		</table>
+		<div className='table-container'>
+			<table>
+				<thead>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<tr key={headerGroup.id}>
+							{headerGroup.headers.map((header) => (
+								<th key={header.id}>
+									{flexRender(
+										header.column.columnDef.header,
+										header.getContext(),
+									)}
+								</th>
+							))}
+						</tr>
+					))}
+				</thead>
+				<tbody>
+					{table.getRowModel().rows.map((row) => (
+						<tr key={row.id}>
+							{row.getVisibleCells().map((cell) => (
+								<td key={cell.id}>
+									{flexRender(cell.column.columnDef.cell, cell.getContext())}
+								</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
 	);
 };
 
